3f9927eb8ca0c1c9f1e7fa8e49f86a16436ffb44
sazkynadeje <zbynek.kasnar@gmail.com>
2026-02-20 21:50:46 +0100

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,65 +1,52 @@
-/**
- * DATABASE SERVICE - KANCELÁŘ NADĚJE v2.0
- * Správa Firebase a operací nad daty
- */
+// js/database.js - Verze 2.0 (Oddělená data)
 
-import { AuthService } from './auth.js';
-
-// Tvůj Firebase Config (ponecháno z tvé verze)
-const firebaseConfig = { 
-    apiKey: "AIzaSyC-NCdmsgA42FxnopKm92m1y5gUGw_z_uE", 
-    authDomain: "nadeje-208bd.firebaseapp.com", 
-    databaseURL: "https://nadeje-208bd-default-rtdb.europe-west1.firebasedatabase.app", 
-    projectId: "nadeje-208bd", 
-    storageBucket: "nadeje-208bd.firebasestorage.app", 
-    messagingSenderId: "688478977789", 
-    appId: "1:688478977789:web:512d1520cf96c9e59cc894" 
+const firebaseConfig = {
+    apiKey: "AIzaSyCxV...", // Tvé API klíče tam nechej tak, jak jsou
+    authDomain: "nadeje-208bd.firebaseapp.com",
+    databaseURL: "https://nadeje-208bd-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nadeje-208bd",
+    storageBucket: "nadeje-208bd.appspot.com",
+    messagingSenderId: "305101689255",
+    appId: "1:305101689255:web:c08343111f15598696ec06"
 };
 
-// Inicializace Firebase (Compat verze pro zachování tvé logiky)
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
+
 const db = firebase.database();
 
 export const DBService = {
-    // Načtení všech zápasů
-    getMatches: (callback) => {
-        db.ref('sazkyData/zapasy').on('value', snap => callback(snap.val()));
-    },
-
-    // Načtení banku
+    // Načtení banku z nové složky
     getBank: (callback) => {
-        db.ref('sazkyData/prevedenyBank').on('value', snap => callback(snap.val() || 0));
+        db.ref('sazkyData_v2/prevedenyBank').on('value', s => callback(s.val() || 0));
     },
 
-    // ODESLÁNÍ TIKETU (Nová verze s ID uživatele)
-    sendTicket: async (cart) => {
-        const user = AuthService.getUser();
-        if (!user) throw new Error("Uživatel není přihlášen");
-
-        const batch = {};
-        const timestamp = Date.now();
-
-        Object.entries(cart).forEach(([matchId, bet]) => {
-            // Sázka se nyní ukládá pod ID uživatele, ne náhodným push klíčem
-            // To zabrání duplicitním sázkám jednoho člověka na jeden zápas
-            batch[`sazkyData/zapasy/${matchId}/sazky/${user.id}`] = {
-                jmeno: user.name,
-                userId: user.id,
-                skore: bet.skore,
-                strelec: bet.strelec,
-                skryt: bet.skryt,
-                stav: "⏳",
-                casZapisu: timestamp
-            };
-        });
-
-        return db.ref().update(batch);
+    // Načtení zápasů z nové složky
+    getMatches: (callback) => {
+        db.ref('sazkyData_v2/zapasy').on('value', s => callback(s.val()));
     },
 
-    // Načtení statistik
+    // Načtení statistik z nové složky
     getStats: (callback) => {
-        db.ref('sazkyData/stats').on('value', snap => callback(snap.val() || {}));
+        db.ref('sazkyData_v2/stats').on('value', s => callback(s.val() || {}));
+    },
+
+    // Odeslání tiketu (ukládá se pod userId do nové složky)
+    sendTicket: async (cart) => {
+        const userData = JSON.parse(localStorage.getItem('nadeje_user'));
+        if (!userData) throw new Error("Nejsi přihlášen!");
+
+        const updates = {};
+        for (const [matchId, bet] of Object.entries(cart)) {
+            const ticketId = db.ref().child(`sazkyData_v2/zapasy/${matchId}/sazky`).push().key;
+            updates[`sazkyData_v2/zapasy/${matchId}/sazky/${ticketId}`] = {
+                ...bet,
+                userId: userData.id,
+                jmeno: userData.name,
+                timestamp: Date.now()
+            };
+        }
+        return db.ref().update(updates);
     }
 };
