bf65c2f64391ce9b75c7baa9e4a4539bda469f88
sazkynadeje <zbynek.kasnar@gmail.com>
2026-02-21 00:18:18 +0100

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,7 +1,8 @@
-// js/database.js - Verze 2.0 (Oddělená data)
+// Na začátek souboru přidej tento import:
+import { AuthService } from './auth.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxV...", // Tvé API klíče tam nechej tak, jak jsou
+    apiKey: "AIzaSyCxV...", // Tvé API klíče tam máš správně
     authDomain: "nadeje-208bd.firebaseapp.com",
     databaseURL: "https://nadeje-208bd-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "nadeje-208bd",
@@ -17,32 +18,33 @@ if (!firebase.apps.length) {
 const db = firebase.database();
 
 export const DBService = {
-    // Načtení banku z nové složky
     getBank: (callback) => {
         db.ref('sazkyData_v2/prevedenyBank').on('value', s => callback(s.val() || 0));
     },
 
-    // Načtení zápasů z nové složky
     getMatches: (callback) => {
         db.ref('sazkyData_v2/zapasy').on('value', s => callback(s.val()));
     },
 
-    // Načtení statistik z nové složky
     getStats: (callback) => {
         db.ref('sazkyData_v2/stats').on('value', s => callback(s.val() || {}));
     },
 
-    // Odeslání tiketu (ukládá se pod userId do nové složky)
+    // OPRAVENÁ FUNKCE ODESLÁNÍ
     sendTicket: async (cart) => {
-        const userData = JSON.parse(localStorage.getItem('nadeje_user'));
-        if (!userData) throw new Error("Nejsi přihlášen!");
+        // Místo localStorage použijeme přímo naši službu
+        const userData = AuthService.getUser(); 
+        
+        if (!userData || !userData.name) {
+            throw new Error("Nejsi přihlášen! Zkus znovu zadat jméno.");
+        }
 
         const updates = {};
         for (const [matchId, bet] of Object.entries(cart)) {
             const ticketId = db.ref().child(`sazkyData_v2/zapasy/${matchId}/sazky`).push().key;
             updates[`sazkyData_v2/zapasy/${matchId}/sazky/${ticketId}`] = {
                 ...bet,
-                userId: userData.id,
+                userId: userData.id || userData.name, // Pojistka pro ID
                 jmeno: userData.name,
                 timestamp: Date.now()
             };
